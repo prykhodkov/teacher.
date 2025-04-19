75a793c9c6627b06b4c79e087ae3713a561625a1
volodymyr <prykhodkov95@gmail.com>
2025-04-19 03:37:05 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,23 @@ let popupOpeners = $('.popup-open');
 let popupClosers = $('.popup-close');
 let dropdowns = $('.dropdown');
 let products = $('.products__list .product');
+let productTitlesList = $('.products__list .product .product__title');
+let courseImgSrc;
+let courseImgName;
+let courseOverviewSrc;
+
+$('main').css('min-height', `calc(100vh - ${$('footer').outerHeight()}px)`);
 
 for (let i = 0; i < popupOpeners.length; i++) {
-  $(popupOpeners[i]).on('click', (e) => {
+  $(popupOpeners[i]).on('click', function() {
+    courseImgSrc = $(this).parent().parent().parent().find('.product__image img').attr('src').split('/');
+    courseImgName = courseImgSrc[courseImgSrc.length - 1].split('.')[0];
+    courseOverviewSrc = `dist/img/${courseImgName}-overview.jpg`;
+
     $('#popup').addClass('show');
     $('body').addClass('lock');
+    $(this).parent().parent().parent().find('.popup__text').clone().appendTo('#popup .popup__info');
+    $('#popup .popup__image img').attr('src', courseOverviewSrc);
 
     setTimeout(() => {
       $('#popup .popup__content').addClass('show');
@@ -15,8 +27,9 @@ for (let i = 0; i < popupOpeners.length; i++) {
 }
 
 for (let i = 0; i < popupClosers.length; i++) {
-  $(popupClosers[i]).on('click', (e) => {
+  $(popupClosers[i]).on('click', function() {
     $('#popup .popup__content').removeClass('show');
+    $('#popup .popup__text').remove();
 
     setTimeout(() => {
       $('#popup').removeClass('show');
@@ -64,25 +77,24 @@ $('.dropdown__option').on('click', function () {
 
 let currentLevelFilter = 'all';
 let currentTypeFilter = 'all';
-let currentFilters = `${currentLevelFilter}-${currentTypeFilter}`;
 
 function filterProductsList() {
   for (let i = 0; i < products.length; i++) {
     $(products[i]).hide();
 
-    if (currentLevelFilter === 'all' && currentTypeFilter === 'all') {
+    if (currentLevelFilter === 'all' && currentTypeFilter === 'all' && $(products[i]).find('.product__title').html().toLowerCase().includes($('#searchFilter').val().toLowerCase())) {
       $(products[i]).show();
     }
 
-    if (currentTypeFilter === 'all' && $(products[i]).data('filter').split('-')[0] === currentLevelFilter) {
+    if (currentTypeFilter === 'all' && $(products[i]).data('levels').split(', ').includes(currentLevelFilter) && $(products[i]).find('.product__title').html().toLowerCase().includes($('#searchFilter').val().toLowerCase())) {
       $(products[i]).show();
     }
 
-    if (currentLevelFilter === 'all' && $(products[i]).data('filter').split('-')[1] === currentTypeFilter) {
+    if (currentLevelFilter === 'all' &&  $(products[i]).data('types').split(', ').includes(currentTypeFilter) && $(products[i]).find('.product__title').html().toLowerCase().includes($('#searchFilter').val().toLowerCase())) {
       $(products[i]).show();
     }
 
-    if ($(products[i]).data('filter') === currentFilters) {
+    if ($(products[i]).data('levels').split(', ').includes(currentLevelFilter) && $(products[i]).data('types').split(', ').includes(currentTypeFilter) && $(products[i]).find('.product__title').html().toLowerCase().includes($('#searchFilter').val().toLowerCase())) {
       $(products[i]).show();
     }
   }
@@ -90,14 +102,40 @@ function filterProductsList() {
 
 $('#filterLevel .dropdown__option').on('click', function () {
   currentLevelFilter = $(this).data('level');
-  currentFilters = `${currentLevelFilter}-${currentTypeFilter}`;
 
   filterProductsList();
 })
 
 $('#filterType .dropdown__option').on('click', function () {
   currentTypeFilter = $(this).data('type');
-  currentFilters = `${currentLevelFilter}-${currentTypeFilter}`;
 
   filterProductsList();
+})
+
+$('#searchIcon').on('click', function() {
+  filterProductsList();
+})
+
+$('#clearIcon').on('click', function() {
+  $('#searchFilter').val('');
+
+  filterProductsList();
+})
+
+$('#searchFilter').on('keypress', function(e) {
+  if (e.key === 'Enter') {
+    filterProductsList();
+  }
+})
+
+$('#navToggle').on('click', () => {
+  $('#navOpen').toggleClass('hide');
+  $('#navClose').toggleClass('hide');
+  $('#nav').toggleClass('slide');
+})
+
+$('#nav').on('blur', function () {
+  $('#navOpen').removeClass('hide');
+  $('#navClose').addClass('hide');
+  $('#nav').removeClass('slide');
 })
